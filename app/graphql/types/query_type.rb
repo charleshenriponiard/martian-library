3541a4ec3cdfa7c09ea5a57ae7bdfad06f7aa175
charles-henri poniard <c.poniard@gmail.com>
2020-04-13 16:09:44 +0200

    mutation sign_in

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,9 +4,15 @@ module Types
           [Types::ItemType],
           null: false,
           description: "Returns a list of items in the martian library"
+    field :me, Types::UserType, null: true
 
     def items
-      Item.all
+      Item.preload(:user)
     end
+
+    def me
+      context[:current_user]
+    end
+    
   end
 end
